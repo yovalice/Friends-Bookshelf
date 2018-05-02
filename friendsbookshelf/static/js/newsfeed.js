d154dd22005fb5faa328dd6bc056367193cd405e
yovalice <yovalice@me.com>
2018-05-02 12:31:32 -0400

    Fix Read more

--- friendsbookshelf/static/js/newsfeed.js
+++ friendsbookshelf/static/js/newsfeed.js
@@ -44,4 +44,10 @@ $(document).ready(function() {
           }
         });
     });
+
+    $('.book-description-text').readmore({
+        speed: 75,
+        collapsedHeight: 192,
+        lessLink: '<a href="#">Read less</a>'
+    });
 });
