4ac1c0afe96d85fe2181e9c62c8edea07b0761b6
yovalice <yovalice@me.com>
2018-05-03 16:32:05 -0400

    Fix user detail read more.

---
+++ friendsbookshelf/static/js/common.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.book-description-text').readmore({
+        speed: 75,
+        collapsedHeight: 192,
+        lessLink: '<a href="#">Read less</a>'
+    });
+});
--- friendsbookshelf/static/js/newsfeed.js
+++ friendsbookshelf/static/js/newsfeed.js
@@ -44,10 +44,4 @@ $(document).ready(function() {
           }
         });
     });
-
-    $('.book-description-text').readmore({
-        speed: 75,
-        collapsedHeight: 192,
-        lessLink: '<a href="#">Read less</a>'
-    });
 });
